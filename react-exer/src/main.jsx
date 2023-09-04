a7ad19ce52a2449480e9e818bb8a9ef2430d0abd
NeugeMa <mariana.n.dourado@gmail.com>
2023-09-04 18:37:54 -0300

    criação códigos + implementação na main

--- react-exer/src/main.jsx
+++ react-exer/src/main.jsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// rotas
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home/index.jsx'
+import Produtos from './routes/Produtos/index.jsx'
+import Error from './routes/Error/index.jsx'
+import EditarProduto from './routes/EditarProdutos/index.jsx'
+// rotas 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
